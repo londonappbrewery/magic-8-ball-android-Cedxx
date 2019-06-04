5b94aa1cc040fcee81f588a5f369bc6e95c8c535
Cedxx <cedmsm@hotmail.com>
2019-06-04 21:42:57 +0200

    small correction in MainActivity.java to make code cleaner

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -17,23 +17,21 @@ public class MainActivity extends AppCompatActivity {
 
         final ImageView ballDisplay = findViewById(R.id.image_eightBall);
 
-        final int [] ballArray = {R.drawable.ball1,
+        final int [] ballArray = new int[] {
+                R.drawable.ball1,
                 R.drawable.ball2,
                 R.drawable.ball3,
                 R.drawable.ball4,
                 R.drawable.ball5,
         };
 
-        Button askButton;
-        askButton = findViewById(R.id.askButton);
+        Button myButton = findViewById(R.id.askButton);
 
-        askButton.setOnClickListener(new View.OnClickListener() {
+        myButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Random randomNumberGenerator = new Random();
-
                 int number = randomNumberGenerator.nextInt(5);
-
                 ballDisplay.setImageResource(ballArray[number]);
             }
         });
